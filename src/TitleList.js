0e5ff472938a7ebc1a377df59adbcb3ba8136d6d
Quentin Ikeno <ikenoq@gmail.com>
2022-08-10 22:37:22 -1000

    Fix typo with className.

--- src/TitleList.js
+++ src/TitleList.js
@@ -4,7 +4,7 @@ const TitleList = ({ posts }) => {
 	if (posts.length === 0) return <p>No posts to display yet!</p>;
 
 	const titles = posts.map((post) => <Title key={post.id} post={post} />);
-	return <div className="columns is-multi-line is-desktop">{titles}</div>;
+	return <div className="columns is-multiline is-desktop">{titles}</div>;
 };
 
 export default TitleList;
