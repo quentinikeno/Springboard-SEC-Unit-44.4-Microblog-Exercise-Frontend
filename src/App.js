adae9fa525ef5b6d1c3a2968449bf3fa7660f41a
Quentin Ikeno <ikenoq@gmail.com>
2022-08-10 17:39:42 -1000

    Create function addPost to add a new post to state and pass down to NewPostForm.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
-import Header from "Header";
+import Header from "./Header";
+import NewPostForm from "./posts/NewPostForm";
 
 function App() {
 	const [posts, setPosts] = useState([]);
+	const addPost = (newPost) => {
+		setPosts([...posts, newPost]);
+	};
 	return (
 		<div className="App">
 			<Header />
 			<Routes>
-				<Route path="/new" element={<NewPostForm />} />
+				<Route
+					path="/new"
+					element={<NewPostForm addPost={addPost} />}
+				/>
 			</Routes>
 		</div>
 	);
