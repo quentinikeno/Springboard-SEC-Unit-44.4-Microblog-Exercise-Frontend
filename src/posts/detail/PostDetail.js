6a1d00f4672a986fae1bafeb12101023b6793ead
Quentin Ikeno <ikenoq@gmail.com>
2022-08-10 18:41:39 -1000

    Fix spacing and formatting for PostDetail.

--- src/posts/detail/PostDetail.js
+++ src/posts/detail/PostDetail.js
@@ -1,22 +1,22 @@
-import React from "react";
-
 const PostDetail = ({ post }) => {
 	const { id, title, description, body } = post;
 	return (
-		<section className="section is-flex is-justify-content-space-between">
-			<div>
-				<h2 className="title is-2">{title}</h2>
-				<p className="subtitle is-2">{description}</p>
+		<section className="section">
+			<div className="is-flex is-justify-content-space-between">
+				<div>
+					<h2 className="title is-2">{title}</h2>
+					<p className="subtitle is-4">{description}</p>
+				</div>
+				<div>
+					<button className="button is-info mr-3">
+						Edit <i className="fa-solid fa-pen-to-square ml-3"></i>
+					</button>
+					<button className="button is-danger">
+						Delete <i className="fa-solid fa-trash ml-3"></i>
+					</button>
+				</div>
 			</div>
-			<div>
-				<button className="button is-info">
-					Edit <i className="fa-solid fa-pen-to-square"></i>
-				</button>
-				<button className="button is-danger">
-					Edit <i className="fa-solid fa-trash"></i>
-				</button>
-			</div>
-			<div>
+			<div className="my-3">
 				<p>{body}</p>
 			</div>
 		</section>
