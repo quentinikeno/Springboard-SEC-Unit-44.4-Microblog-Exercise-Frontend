f27dd294d24a4443e7cfdf3d67906df1ac2dbb1a
Quentin Ikeno <ikenoq@gmail.com>
2022-08-11 18:23:18 -1000

    Create form to edit posts.

---
+++ src/posts/EditPostForm.js
@@ -0,0 +1,81 @@
+import useFormState from "../hooks/useFormState";
+import { Link, useNavigate } from "react-router-dom";
+
+const EditPostForm = ({ post }) => {
+	const initialState = post;
+	const [formData, setFormData, handleChange] = useFormState(initialState);
+
+	const navigate = useNavigate();
+
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		addPost({ ...formData, id: uuid() });
+		navigate("/");
+	};
+
+	return (
+		<form onSubmit={handleSubmit} className="my-5">
+			<div className="field">
+				<label htmlFor="title" className="label">
+					Title
+				</label>
+				<div className="control">
+					<input
+						type="text"
+						className="input"
+						onChange={handleChange}
+						id="title"
+						name="title"
+						value={formData.title}
+						required
+					/>
+				</div>
+			</div>
+			<div className="field">
+				<label htmlFor="description" className="label">
+					Description
+				</label>
+				<div className="control">
+					<input
+						type="text"
+						className="input"
+						onChange={handleChange}
+						id="description"
+						name="description"
+						value={formData.description}
+						required
+					/>
+				</div>
+			</div>
+			<div className="field">
+				<label htmlFor="body" className="label">
+					Body
+				</label>
+				<div className="control">
+					<textarea
+						className="textarea"
+						onChange={handleChange}
+						id="body"
+						name="body"
+						value={formData.body}
+						required
+					/>
+				</div>
+			</div>
+			<div className="field is-grouped">
+				<div className="control">
+					<button type="submit" className="button is-link">
+						Save
+					</button>
+				</div>
+				<div className="control">
+					<Link to="/" className="button is-link is-light">
+						Cancel
+					</Link>
+				</div>
+			</div>
+		</form>
+	);
+};
+
+export default EditPostForm;
