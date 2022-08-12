94dacb9286db2e7eaa13e12361a7e5f96bc4f6a0
Quentin Ikeno <ikenoq@gmail.com>
2022-08-11 18:11:37 -1000

    Add function to editPost.

--- src/App.js
+++ src/App.js
@@ -5,12 +5,17 @@ import AppRoutes from "./routes/AppRoutes";
 function App() {
 	const [posts, setPosts] = useState([]);
 	const addPost = (newPost) => {
-		setPosts([...posts, newPost]);
+		setPosts((posts) => [...posts, newPost]);
+	};
+	const editPost = (editedPost) => {
+		setPosts((posts) =>
+			posts.map((post) => (post.id === editedPost.id ? editedPost : post))
+		);
 	};
 	return (
 		<div className="App container">
 			<Header />
-			<AppRoutes posts={posts} addPost={addPost} />
+			<AppRoutes posts={posts} addPost={addPost} editPost={editPost} />
 		</div>
 	);
 }
