f6a2978687512518c2fd557cca67f3e12fd6f4f9
Quentin Ikeno <ikenoq@gmail.com>
2022-08-13 12:59:41 -1000

    Find comment with the postId from the route params and pass in down to the PostDetail.

--- src/posts/detail/FindPost.js
+++ src/posts/detail/FindPost.js
@@ -1,15 +1,26 @@
 import { useParams, Navigate } from "react-router-dom";
 import PostDetail from "./PostDetail";
 
-const FindPost = ({ posts, editPost, deletePost }) => {
+const FindPost = ({
+	posts,
+	editPost,
+	deletePost,
+	postComments,
+	addComment,
+	deleteComment,
+}) => {
 	const { postId } = useParams();
 	const foundPost = posts.find((post) => post.id === postId);
+	const foundComments = postComments[postId];
 
 	return foundPost ? (
 		<PostDetail
 			post={foundPost}
 			editPost={editPost}
 			deletePost={deletePost}
+			comments={foundComments}
+			addComment={addComment}
+			deleteComment={deleteComment}
 		/>
 	) : (
 		<Navigate to="/" />
