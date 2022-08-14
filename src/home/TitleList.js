f44633a0ecafe4d18049dcebcd3ebbc652d831dc
Quentin Ikeno <ikenoq@gmail.com>
2022-08-13 17:48:14 -1000

    Update to get posts from redux store.

--- src/home/TitleList.js
+++ src/home/TitleList.js
@@ -1,9 +1,20 @@
+import { useSelector } from "react-redux";
 import Title from "./Title";
 
-const TitleList = ({ posts }) => {
-	if (posts.length === 0) return <p>No posts to display yet!</p>;
+const TitleList = () => {
+	const posts = useSelector((state) => state.posts);
+	const postIds = Object.key(posts);
 
-	const titles = posts.map((post) => <Title key={post.id} post={post} />);
+	if (postIds.length === 0) return <p>No posts to display yet!</p>;
+
+	const titles = postIds.map((id) => (
+		<Title
+			key={id}
+			id={id}
+			title={posts[id].title}
+			description={posts[id].description}
+		/>
+	));
 	return <div className="columns is-multiline is-desktop">{titles}</div>;
 };
 
