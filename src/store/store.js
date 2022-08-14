64912176b33eee6b86a461000374f2304aaa2d02
Quentin Ikeno <ikenoq@gmail.com>
2022-08-14 11:11:15 -1000

    Add commentsReducer to store.

--- src/store/store.js
+++ src/store/store.js
@@ -1,4 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import postsReducer from "./postsSlice";
+import commentsReducer from "./commentsSlice";
 
-export const store = configureStore({ reducer: { posts: postsReducer } });
+export const store = configureStore({
+	reducer: { posts: postsReducer, comments: commentsReducer },
+});
