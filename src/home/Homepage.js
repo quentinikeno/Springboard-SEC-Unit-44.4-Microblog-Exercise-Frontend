3c2c2d2f564382373686217805df0325b39409f4
Quentin Ikeno <ikenoq@gmail.com>
2022-08-13 17:41:35 -1000

    Remove props for posts.

--- src/home/Homepage.js
+++ src/home/Homepage.js
@@ -1,13 +1,13 @@
 import TitleList from "./TitleList";
 
-const Homepage = ({ posts }) => {
+const Homepage = () => {
 	return (
 		<div>
 			<p className="my-5">
 				Welcome to <strong>Microblog</strong>, our innovative site for
 				communicating on the information superhighway.
 			</p>
-			<TitleList posts={posts} />
+			<TitleList />
 		</div>
 	);
 };
