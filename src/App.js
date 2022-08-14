dac5e46c97c912c4721de3503820b972e5ce6ead
Quentin Ikeno <ikenoq@gmail.com>
2022-08-14 11:12:27 -1000

    Remove local state for comments for App.

--- src/App.js
+++ src/App.js
@@ -1,44 +1,11 @@
 import Header from "./Header";
 import AppRoutes from "./routes/AppRoutes";
 
-import { useState } from "react";
-
 function App() {
-	const [postComments, setPostComments] = useState({});
-
-	const initComment = (postId) => {
-		setPostComments((postComments) => ({ ...postComments, [postId]: {} }));
-	};
-	const addComment = (postId, commentId, text) => {
-		setPostComments((postComments) => {
-			const comments = { ...postComments[postId] };
-			const newPostComments = {
-				...postComments,
-				[postId]: { ...comments, [commentId]: text },
-			};
-			return newPostComments;
-		});
-	};
-	const deleteComment = (postId, commentId) => {
-		setPostComments((postComments) => {
-			const comments = { ...postComments[postId] };
-			delete comments[commentId];
-			const newPostComments = {
-				...postComments,
-				[postId]: comments,
-			};
-			return newPostComments;
-		});
-	};
 	return (
 		<div className="App container">
 			<Header />
-			<AppRoutes
-				postComments={postComments}
-				initComment={initComment}
-				addComment={addComment}
-				deleteComment={deleteComment}
-			/>
+			<AppRoutes />
 		</div>
 	);
 }
