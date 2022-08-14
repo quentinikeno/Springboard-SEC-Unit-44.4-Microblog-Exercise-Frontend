0df11195b022411f6e3b077b442f20929d637ac6
Quentin Ikeno <ikenoq@gmail.com>
2022-08-13 17:39:29 -1000

    Remove local state for posts.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,8 @@
-import { useState } from "react";
 import Header from "./Header";
 import AppRoutes from "./routes/AppRoutes";
 
 function App() {
-	const [posts, setPosts] = useState([]);
 	const [postComments, setPostComments] = useState({});
-	// {postId: {commentId: text, ...}}
-
-	const addPost = (newPost) => {
-		setPosts((posts) => [...posts, newPost]);
-	};
-	const editPost = (editedPost) => {
-		setPosts((posts) =>
-			posts.map((post) => (post.id === editedPost.id ? editedPost : post))
-		);
-	};
-	const deletePost = (id) => {
-		setPosts((posts) => [...posts].filter((post) => post.id !== id));
-	};
 
 	const initComment = (postId) => {
 		setPostComments((postComments) => ({ ...postComments, [postId]: {} }));
@@ -47,10 +32,6 @@ function App() {
 		<div className="App container">
 			<Header />
 			<AppRoutes
-				posts={posts}
-				addPost={addPost}
-				editPost={editPost}
-				deletePost={deletePost}
 				postComments={postComments}
 				initComment={initComment}
 				addComment={addComment}
