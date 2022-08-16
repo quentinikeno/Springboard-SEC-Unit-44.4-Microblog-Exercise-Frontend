287590b11a6f1d950696900b7c82fffbc564fb82
Quentin Ikeno <ikenoq@gmail.com>
2022-08-15 18:02:41 -1000

    Add slice with async thunk for titles.

---
+++ src/store/titlesSlice.js
@@ -0,0 +1,46 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const titlesURL = "http://localhost:5000/api/posts";
+
+const initialState = {
+	titles: {},
+	isLoading: false,
+};
+
+export const fetchTitles = createAsyncThunk("titles/fetchTitles", async () => {
+	try {
+		const response = await axios.get(titlesURL);
+		// Data from API will be in the form of [{ id, title, description, votes, }, ...]
+		// Change it to be in the form of {id: {title, description, votes}, ...}
+		const titleData = response.data.reduce((previous, current) => {
+			const { id, title, description, votes } = current;
+			previous[id] = { title, description, votes };
+		}, {});
+		return titleData;
+	} catch (error) {
+		return err.message;
+	}
+});
+
+export const titlesSlice = createSlice({
+	name: "titles",
+	initialState,
+	reducers: {},
+	extraReducers: (builder) => {
+		builder
+			.addCase(fetchTitles.pending, (state) => {
+				state.isLoading = true;
+			})
+			.addCase(fetchTitles.fulfilled, (state, action) => {
+				state.isLoading = false;
+				state.titles = action.payload;
+			})
+			.addCase(fetchTitles.rejected, (state, action) => {
+				state.isLoading = false;
+				state.titles = action.payload;
+			});
+	},
+});
+
+export default titlesSlice.reducer;
