97be8c91f60e2b56044d948cff04c45ccf27bc01
Quentin Ikeno <ikenoq@gmail.com>
2022-08-15 19:09:39 -1000

    Find the post ID in the state for titles instead of posts.

--- src/posts/detail/FindPost.js
+++ src/posts/detail/FindPost.js
@@ -4,7 +4,8 @@ import PostDetail from "./PostDetail";
 
 const FindPost = () => {
 	const { postId } = useParams();
-	const foundPost = useSelector((state) => state.posts[postId]) || null;
+	const foundPost =
+		useSelector((state) => state.titles.titles[postId]) || null;
 
 	return foundPost ? <PostDetail postId={postId} /> : <Navigate to="/" />;
 };
