aa438d36a5a03e76d01f754af722a64ea2d3f598
Quentin Ikeno <ikenoq@gmail.com>
2022-08-15 19:06:45 -1000

    Add thunk to get post details.

--- src/store/postsSlice.js
+++ src/store/postsSlice.js
@@ -1,7 +1,19 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
 
-// {postId: {title, description, body}, ...}
-const initialState = {};
+const postURL = "http://localhost:5000/api/posts";
+// posts: {postId: {title, description, body}, ...}
+const initialState = { posts: {}, isLoading: false };
+
+export const getPost = createAsyncThunk("posts/getPosts", async (postId) => {
+	try {
+		const response = await axios.get(`${postURL}/${postId}`);
+		// { id, title, description, body, votes, comments: [ { id, text }, ... ], }
+		return response.data;
+	} catch (error) {
+		return error.message;
+	}
+});
 
 export const postsSlice = createSlice({
 	name: "posts",
@@ -9,16 +21,32 @@ export const postsSlice = createSlice({
 	reducers: {
 		addPost: (state, action) => {
 			const { id, title, description, body } = action.payload;
-			state[id] = { title, description, body };
+			state.posts[id] = { title, description, body };
 		},
 
 		editPost: (state, action) => {
 			const { id, title, description, body } = action.payload;
-			state[id] = { title, description, body };
+			state.posts[id] = { title, description, body };
 		},
 		deletePost: (state, action) => {
-			delete state[action.payload];
+			delete state.posts[action.payload];
 		},
+	},
+	extraReducers: (builder) => {
+		builder
+			.addCase(getPost.pending, (state) => {
+				state.isLoading = true;
+			})
+			.addCase(getPost.fulfilled, (state, action) => {
+				state.isLoading = false;
+				const { id, title, description, body, votes, comments } =
+					action.payload;
+				state.posts[id] = { title, description, body, votes, comments };
+			})
+			.addCase(getPost.rejected, (state, action) => {
+				state.isLoading = false;
+				state.posts = action.payload;
+			});
 	},
 });
 
