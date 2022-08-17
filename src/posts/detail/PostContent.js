409fcd735b010ae95a780d0d11fd961b93969100
Quentin Ikeno <ikenoq@gmail.com>
2022-08-16 18:54:01 -1000

    dispatch action to delete post from API when the delete button is clicked.

--- src/posts/detail/PostContent.js
+++ src/posts/detail/PostContent.js
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
-import { deletePost, getPostFromAPI } from "../../store/postsSlice";
+import { deletePostFromAPI, getPostFromAPI } from "../../store/postsSlice";
 import CommentsSection from "../comments/CommentsSection";
 
 const PostContent = ({ postId, toggleIsEditing }) => {
@@ -22,7 +22,7 @@ const PostContent = ({ postId, toggleIsEditing }) => {
 	const { title, description, body, comments } = post;
 
 	const handleDelete = () => {
-		dispatch(deletePost(postId));
+		dispatch(deletePostFromAPI(postId));
 		navigate("/");
 	};
 
