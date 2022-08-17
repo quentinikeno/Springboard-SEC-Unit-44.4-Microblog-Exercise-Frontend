40d43f311be0f0f8ee7bfa60f874e3f999ed89b5
Quentin Ikeno <ikenoq@gmail.com>
2022-08-16 15:35:38 -1000

    Delete slice for comments and remove it from the store.

--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import postsReducer from "./postsSlice";
-import commentsReducer from "./commentsSlice";
 import titlesReducer from "./titlesSlice";
 
 export const store = configureStore({
 	reducer: {
 		posts: postsReducer,
-		comments: commentsReducer,
 		titles: titlesReducer,
 	},
 });
