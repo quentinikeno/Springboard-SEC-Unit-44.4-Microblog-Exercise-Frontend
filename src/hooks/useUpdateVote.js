e52ac7a134f294cb7837872e7c6e9159a0b9a461
Quentin Ikeno <ikenoq@gmail.com>
2022-08-18 09:13:07 -1000

    handleVote updates the votes in the state for posts.

--- src/hooks/useUpdateVote.js
+++ src/hooks/useUpdateVote.js
@@ -1,16 +1,19 @@
 import { useDispatch } from "react-redux";
 import { updateVoteToAPITitle } from "../store/titlesSlice";
-import { updateVoteToAPIPost } from "../store/postsSlice";
+import { updateVoteToAPIPost, incDecVotes } from "../store/postsSlice";
 
 const useUpdateVote = (postId, isPost) => {
 	// isPost: boolean checking if this is a post.  If it is a post, we need to dispatch an action to the post slice.
 	// If not dispatch an action to the titles slice.
 	const dispatch = useDispatch();
 
-	const handleVote = (direction) => {
-		isPost
-			? dispatch(updateVoteToAPIPost({ postId, direction }))
-			: dispatch(updateVoteToAPITitle({ postId, direction }));
+	const handleVote = async (direction) => {
+		if (isPost) {
+			await dispatch(updateVoteToAPIPost({ postId, direction }));
+		} else {
+			await dispatch(updateVoteToAPITitle({ postId, direction }));
+			dispatch(incDecVotes({ postId, direction }));
+		}
 	};
 	return handleVote;
 };
